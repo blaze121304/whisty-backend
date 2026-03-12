7c1099b295551177a5c2b4535849ae7caf1293e2
rusty <blaze121304@gmail.com>
2026-03-12 16:11:15 +0900

    2026.03.06 sql 수정 - 테스트

--- src/main/java/com/rusty/whiskeybackend/common/config/CorsConfig.java
+++ src/main/java/com/rusty/whiskeybackend/common/config/CorsConfig.java
@@ -11,7 +11,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:10007")
+                .allowedOrigins("http://localhost:10007", "http://192.168.0.206:10007")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
